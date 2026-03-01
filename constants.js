9225641b8dbe6c997b96c4699c13225c27b254cd
raza-h <razah12145@gmail.com>
2026-03-01 20:09:17 +0500

    change: include saturday as a business day for Pakistan

--- constants.js
+++ constants.js
@@ -6,8 +6,7 @@ export const THRESHOLD = process.env.THRESHOLD || 5000;
 export const CRON_OPTIONS = { timezone: "Asia/Karachi" };
 export const FROM_NUMBER = `whatsapp:${process.env.TWILIO_FROM_NUMBER}`;
 export const CRON_EXPRESSIONS = [
-    "*/5 18-21 * * 1-5",
-    "*/15 13-17,22-23,0-3 * * 1-5",
-    "0 4-12 * * 1-5",
-    "*/15 0-3 * * 6",
+    "*/5 18-21 * * 1-6",
+    "*/15 13-17,22-23,0-3 * * 1-6",
+    "0 4-12 * * 1-6",
 ];
